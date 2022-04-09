afd3f06d3b58dac9fb526f72710e5cbed997f6a7
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-04-09 16:11:36 -0300

    FEATURE: Replaced hello world controller

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/WebController.java
@@ -0,0 +1,60 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.interfaces.services.DebateService;
+import ar.edu.itba.paw.interfaces.services.PostService;
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.webapp.exception.DebateNotFoundException;
+import ar.edu.itba.paw.webapp.form.PostForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class WebController {
+
+    private final UserService userService;
+    private final DebateService debateService;
+    private final PostService postService;
+
+    @Autowired
+    public WebController(UserService userService, DebateService debateService, PostService postService) {
+        this.userService = userService;
+        this.debateService = debateService;
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.HEAD })
+    public ModelAndView debatesList() {
+        final ModelAndView mav = new ModelAndView("debates-list");
+//        mav.addObject("debates", debateService.getAll().orElseThrow(DebateNotFoundException::new)); // TODO get all
+        return mav;
+    }
+
+    @RequestMapping(value = "/debate/{debateId}", method = { RequestMethod.GET, RequestMethod.HEAD })
+    public ModelAndView debate(@PathVariable("debateId") final long debateId, @ModelAttribute("postForm") final PostForm form) {
+        final ModelAndView mav = new ModelAndView("debate");
+        mav.addObject("debate", debateService.getDebateById(debateId)/*.orElseThrow(DebateNotFoundException::new)*/); // TODO uncomment later
+//        mav.addObject("posts", postService.getPostByDebate(debateId)); // TODO get post by debate
+        return mav;
+    }
+
+    @RequestMapping(value = "/debate/{debateId}", method = { RequestMethod.POST })
+    public ModelAndView createPost(@PathVariable("debateId") final long debateId, @Valid @ModelAttribute("postForm") final PostForm form, BindingResult errors) {
+        if (errors.hasErrors()) {
+            return debate(debateId, form);
+        }
+//        postService.createEmail(form.getEmail(), debateId, form.getContent()); // TODO create with email
+        return new ModelAndView("redirect:/debate/" + debateId);
+    }
+
+    @ExceptionHandler(DebateNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ModelAndView handleUserNotFoundException() {
+        return new ModelAndView("error/404");
+    }
+}
