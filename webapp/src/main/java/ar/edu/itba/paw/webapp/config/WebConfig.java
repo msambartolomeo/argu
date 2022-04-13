dbfa3431f2f25155bd802ab1c7728a055eeb392e
Azul <azkim@itba.edu.ar>
2022-04-12 23:36:06 -0300

    PATCH: Forgot to add the property referring to the port

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -88,7 +88,7 @@ public class WebConfig {
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(env.getProperty("spring.mail.host"));
-        mailSender.setPort(587);
+        mailSender.setPort(Integer.parseInt(env.getProperty("spring.mail.port")));
 
         mailSender.setUsername(env.getProperty("spring.mail.username"));
         mailSender.setPassword(env.getProperty("spring.mail.password"));
