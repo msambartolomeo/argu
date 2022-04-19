70b1a724a154e67a9d3bbbda7e9fbfad5746c51c
Juan Negro <jnegro@itba.edu.ar>
2022-04-19 00:33:45 -0300

    FEATURE: Boilerplate Spring Security

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -0,0 +1,64 @@
+package ar.edu.itba.paw.webapp.config;
+
+import ar.edu.itba.paw.webapp.auth.PawUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("ar.edu.itba.paw.webapp.auth")
+public class WebAuthConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PawUserDetailsService userDetailsService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                    .invalidSessionUrl("/login")
+                .and().authorizeRequests()
+                    .antMatchers("/login", "/register").anonymous()
+                    .antMatchers("/**").authenticated()
+                .and().formLogin()
+                    .usernameParameter("j_username")
+                    .passwordParameter("j_password")
+                    .defaultSuccessUrl("/", false)
+                    .loginPage("/login")
+                .and().rememberMe()
+                    .rememberMeParameter("j_rememberme")
+                    .userDetailsService(userDetailsService)
+                    // TODO: Usar OpenSSL para generar un String aleatorio de 4K u 8K para usar como key
+                    .key("mysupersecretketthatnobodyknowsabout")
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+                .and().logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login")
+                .and().exceptionHandling()
+                    .accessDeniedPage("/403")
+                .and().csrf().disable();
+    }
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring() // Ignore static resources
+                .antMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico", "/403");
+    }
+}
