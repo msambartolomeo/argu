fb4e1b8e99c8f3cd698c8d7a122465ac4c6b7afb
Juan Negro <jnegro@itba.edu.ar>
2022-04-21 20:40:12 -0300

    PATCH: Fixed Resources in SpringSecurity

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -59,6 +59,6 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.ignoring() // Ignore static resources
-                .antMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico", "/403");
+                .antMatchers("/resources/**", "/403");
     }
 }
