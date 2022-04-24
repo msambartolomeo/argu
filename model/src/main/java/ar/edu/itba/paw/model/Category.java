4bf64f7547868d1479f727875dd4c5d8c30c0f1a
Azul <azkim@itba.edu.ar>
2022-04-24 19:04:38 -0300

    FEATURE: Add Category enum
    Co-authored-by: Juan <jdeluca@itba.edu.ar>

---
+++ model/src/main/java/ar/edu/itba/paw/model/Category.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.paw.model;
+
+public enum Category {
+    CULTURE,
+    ECONOMICS,
+    EDUCATION,
+    ENTERTAINMENT,
+    HISTORY,
+    LITERATURE,
+    POLITICS,
+    RELIGION,
+    SCIENCE,
+    TECHNOLOGY,
+    WORLD
+}
