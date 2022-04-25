56ad98a12d3106881da8acac931f5ced31e47a41
Juan Negro <jnegro@itba.edu.ar>
2022-04-24 21:18:18 -0300

    PATCH: Fixed Authorities Error

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
@@ -9,12 +9,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.regex.Pattern;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class PawUserDetailsService implements UserDetailsService {
@@ -25,8 +23,8 @@ public class PawUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         final User user = us.getUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No user by the name " + username));
-        final Collection<GrantedAuthority> authorities =
-                Arrays.asList(new SimpleGrantedAuthority("USER"));
+        final Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
         if (user.getRole() == UserRole.MODERATOR) {
             authorities.add(new SimpleGrantedAuthority("MODERATOR"));
         }
