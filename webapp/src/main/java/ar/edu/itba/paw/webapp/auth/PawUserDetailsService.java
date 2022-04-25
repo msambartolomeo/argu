64d5a6c9aace5cb11362c6ab786312c224363936
Juan Negro <jnegro@itba.edu.ar>
2022-04-24 21:10:17 -0300

    FEATURE: User Roles basic implementation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.webapp.auth;
 
 import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,19 +21,15 @@ public class PawUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserService us;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    private final static Pattern BYCRYPT_PATTERN = Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}");
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         final User user = us.getUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No user by the name " + username));
-        final Collection<? extends GrantedAuthority> authorities =
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-
-        // Migrate users with un-hashed passwords
-        //if(!BYCRYPT_PATTERN.matcher(user.getPassword()).matches())
-            // TODO: Update user password
+        final Collection<GrantedAuthority> authorities =
+                Arrays.asList(new SimpleGrantedAuthority("USER"));
+        if (user.getRole() == UserRole.MODERATOR) {
+            authorities.add(new SimpleGrantedAuthority("MODERATOR"));
+        }
 
         return new org.springframework.security.core.userdetails.User(username, user.getPassword(), authorities);
     }
