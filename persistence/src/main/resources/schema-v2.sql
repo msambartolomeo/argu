89754b23553dc086c817ba7c25ca3859b532700e
Juan Negro <jnegro@itba.edu.ar>
2022-05-07 10:59:59 -0300

    PATCH: Fixed SchemaV1 View Error

--- persistence/src/main/resources/schema-v2.sql
+++ persistence/src/main/resources/schema-v2.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS votes (
 );
 
 CREATE OR REPLACE VIEW public_debates AS
-SELECT debates.debateid, debates.name, debates.description, debates.category, debates.created_date, debates.imageid,
+    SELECT debates.debateid, debates.name, debates.description, debates.category, debates.created_date, debates.imageid,
        u1.username AS creatorusername, u2.username AS opponentusername,
        COUNT(DISTINCT s.userid) AS subscribedcount, debates.status,
        COUNT(DISTINCT v.userid) FILTER ( WHERE vote = 0 ) AS forcount, COUNT(DISTINCT v.userid) FILTER ( WHERE vote = 1 ) AS againstcount
-FROM debates LEFT JOIN users u1 ON debates.creatorid = u1.userid
-    LEFT JOIN users u2 ON debates.opponentid = u2.userid LEFT JOIN subscribed s on debates.debateid = s.debateid
-    LEFT JOIN votes v ON debates.debateid = v.debateid
-GROUP BY debates.debateid, u1.userid, u2.userid;
+    FROM debates LEFT JOIN users u1 ON debates.creatorid = u1.userid
+        LEFT JOIN users u2 ON debates.opponentid = u2.userid LEFT JOIN subscribed s on debates.debateid = s.debateid
+        LEFT JOIN votes v ON debates.debateid = v.debateid
+    GROUP BY debates.debateid, u1.userid, u2.userid;
