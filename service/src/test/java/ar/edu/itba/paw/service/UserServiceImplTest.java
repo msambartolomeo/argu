cbcc3c041f6271f65840fc7692ea32f74b24c226
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-05-10 01:05:55 -0300

    TEST: Added user service tests

--- service/src/test/java/ar/edu/itba/paw/service/UserServiceImplTest.java
+++ service/src/test/java/ar/edu/itba/paw/service/UserServiceImplTest.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.interfaces.dao.UserDao;
+import ar.edu.itba.paw.interfaces.services.EmailService;
+import ar.edu.itba.paw.interfaces.services.ImageService;
+import ar.edu.itba.paw.model.Image;
 import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.model.enums.UserRole;
 import org.junit.Test;
@@ -9,7 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
@@ -24,14 +26,22 @@ public class UserServiceImplTest {
     private final static String USER_USERNAME = "username";
     private final static String USER_PASSWORD = "password";
     private final static String USER_EMAIL = "test@test.com";
+    private final static String MODERATOR_REASON = "reason";
     private final static LocalDate USER_DATE = LocalDate.parse("2022-01-01");
     private final static UserRole USER_ROLE = UserRole.USER;
+    private final static byte[] IMAGE_DATA = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    private final static long IMAGE_ID = 1;
+
     @InjectMocks
     private UserServiceImpl userService = new UserServiceImpl();
     @Mock
     private UserDao userDao;
     @Mock
     private PasswordEncoder passwordEncoder;
+    @Mock
+    private EmailService emailService;
+    @Mock
+    private ImageService imageService;
 
     @Test
     public void testCreateUserNew() {
@@ -46,5 +56,99 @@ public class UserServiceImplTest {
         assertEquals(user.getPassword(), u.getPassword());
         assertEquals(user.getEmail(), u.getEmail());
         assertEquals(user.getRole(), u.getRole());
+    }
+
+    @Test
+    public void testCreateUserUpdateLegacy() {
+        User user = new User(USER_ID, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_DATE, USER_ROLE);
+        Mockito.when(userDao.updateLegacyUser(Mockito.anyLong(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString())).thenReturn(user);
+        Mockito.when(userDao.getUserByEmail(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn(USER_PASSWORD);
+
+        User u = userService.create(USER_USERNAME, USER_PASSWORD, USER_EMAIL);
+
+        assertEquals(user.getUsername(), u.getUsername());
+        assertEquals(user.getPassword(), u.getPassword());
+        assertEquals(user.getEmail(), u.getEmail());
+        assertEquals(user.getRole(), u.getRole());
+    }
+
+    @Test
+    public void testGetUserByUsername() {
+        User user = new User(USER_ID, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_DATE, USER_ROLE);
+        Mockito.when(userDao.getUserByUsername(Mockito.anyString())).thenReturn(Optional.of(user));
+
+        Optional<User> u = userService.getUserByUsername(USER_USERNAME);
+
+        assertTrue(u.isPresent());
+        assertEquals(user.getUsername(), u.get().getUsername());
+        assertEquals(user.getPassword(), u.get().getPassword());
+        assertEquals(user.getEmail(), u.get().getEmail());
+        assertEquals(user.getRole(), u.get().getRole());
+    }
+
+    @Test
+    public void testGetUserByUsernameEmpty() {
+        Mockito.when(userDao.getUserByUsername(Mockito.anyString())).thenReturn(Optional.empty());
+
+        Optional<User> u = userService.getUserByUsername(USER_USERNAME);
+
+        assertFalse(u.isPresent());
+    }
+
+    @Test
+    public void testGetUserByEmail() {
+        User user = new User(USER_ID, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_DATE, USER_ROLE);
+        Mockito.when(userDao.getUserByEmail(Mockito.anyString())).thenReturn(Optional.of(user));
+
+        Optional<User> u = userService.getUserByEmail(USER_EMAIL);
+
+        assertTrue(u.isPresent());
+        assertEquals(user.getUsername(), u.get().getUsername());
+        assertEquals(user.getPassword(), u.get().getPassword());
+        assertEquals(user.getEmail(), u.get().getEmail());
+        assertEquals(user.getRole(), u.get().getRole());
+    }
+
+    @Test
+    public void testGetUserByEmailEmpty() {
+        Mockito.when(userDao.getUserByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+
+        Optional<User> u = userService.getUserByEmail(USER_EMAIL);
+
+        assertFalse(u.isPresent());
+    }
+
+    @Test
+    public void testRequestModerator() {
+        userService.requestModerator(USER_USERNAME, MODERATOR_REASON);
+
+        Mockito.verify(emailService).sendEmailSelf(Mockito.anyString(), Mockito.anyString());
+    }
+
+    @Test
+    public void testUpdateImage() {
+        User user = new User(USER_ID, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_DATE, IMAGE_ID, USER_ROLE);
+        Image image = new Image(IMAGE_ID, IMAGE_DATA);
+        Mockito.when(userDao.getUserByUsername(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(imageService.createImage(Mockito.any())).thenReturn(image);
+
+        userService.updateImage(USER_USERNAME, IMAGE_DATA);
+
+        Mockito.verify(userDao).updateImage(Mockito.anyLong(), Mockito.anyLong());
+        Mockito.verify(imageService).deleteImage(Mockito.anyLong());
+    }
+
+    @Test
+    public void testUpdateImageNoPreviousImage() {
+        User user = new User(USER_ID, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_DATE, USER_ROLE);
+        Image image = new Image(IMAGE_ID, IMAGE_DATA);
+        Mockito.when(userDao.getUserByUsername(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(imageService.createImage(Mockito.any())).thenReturn(image);
+
+        userService.updateImage(USER_USERNAME, IMAGE_DATA);
+
+        Mockito.verify(userDao).updateImage(Mockito.anyLong(), Mockito.anyLong());
+        Mockito.verify(imageService, Mockito.times(0)).deleteImage(Mockito.anyLong());
     }
 }
