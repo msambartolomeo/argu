81614c1d06f5cfc5793fa653759118be22d4cc0a
Juan <jdeluca@itba.edu.ar>
2022-05-25 15:58:31 -0300

    CHORE: Basic Image Jpa functionality

--- model/src/main/java/ar/edu/itba/paw/model/Image.java
+++ model/src/main/java/ar/edu/itba/paw/model/Image.java
@@ -1,15 +1,35 @@
 package ar.edu.itba.paw.model;
 
-public class Image {
-    private final long id;
-    private final byte[] data;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "images_imageid_seq")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "images_imageid_seq", name = "images_imageid_seq")
+    @Column(name = "imageid")
+    private Long id;
+
+    @Column(nullable = false)
+    private byte[] data;
+
+    /* default */ Image() {
+        // just hibernate
+    }
+
+    public Image(byte[] data) {
+        this.data = data;
+    }
+
+    // TODO deprecate
     public Image(long id, byte[] data) {
         this.id = id;
         this.data = data;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
--- persistence/src/main/java/ar/edu/itba/paw/persistence/ImageJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ImageJdbcDao.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
+//@Repository
+@Deprecated
 public class ImageJdbcDao implements ImageDao {
 
     private final JdbcTemplate jdbcTemplate;
