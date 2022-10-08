0c0d11cd7c046bc5519ad101766fdd6f7717291a
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-10-08 01:00:41 -0300

    FEATURE: Add pagination first implementation

---
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,50 @@
+import "./Pagination.css";
+import cn from "classnames";
+
+interface Props {
+    param: string;
+    totalPages: number;
+}
+
+function Pagination({ param, totalPages }: Props) {
+    // TODO: Get page number from router
+    const currentPage = 0;
+
+    // TODO: Get url from router and keep current params
+    const getUrl = (page: number) => `http://localhost:3000?${param}=${page}`;
+
+    const buttons = [];
+    for (let i = 0; i < totalPages; i++) {
+        buttons.push(
+            <li className={cn({ "selected-button": i === currentPage })}>
+                <a href={getUrl(i)}>{i}</a>
+            </li>
+        );
+    }
+
+    return (
+        <>
+            {totalPages > 0 && (
+                <div className="center pagination-margin">
+                    <ul className="pagination">
+                        {currentPage > 0 && (
+                            <li>
+                                <a href={getUrl(currentPage - 1)}>{"<"}</a>
+                            </li>
+                        )}
+
+                        {buttons}
+
+                        {currentPage < totalPages && (
+                            <li>
+                                <a href={getUrl(currentPage + 1)}>{">"}</a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default Pagination;
