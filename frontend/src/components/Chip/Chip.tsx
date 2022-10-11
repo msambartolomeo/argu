772f8ebc751da36382cf133008db515a6506a2f6
Azul <azkim@itba.edu.ar>
2022-10-11 17:08:14 -0300

    PATCH: Change non-clickable chips to generic chip

---
+++ frontend/src/components/Chip/Chip.tsx
@@ -0,0 +1,22 @@
+import "./Chip.css";
+
+interface ChipProps {
+    name: string;
+    path?: string;
+}
+
+const Chip = ({ name, path }: ChipProps) => {
+    return (
+        <>
+            <a
+                className="chip btn waves-effect"
+                style={{ pointerEvents: path ? "auto" : "none" }}
+                href={path}
+            >
+                {name}
+            </a>
+        </>
+    );
+};
+
+export default Chip;
