10e32a69f0b8c4866b916cc38713ca01e99972e9
Azul <azkim@itba.edu.ar>
2022-11-13 20:29:51 -0300

    FEATURE: Add debate photo component

---
+++ frontend/src/components/DebatePhoto/DebatePhoto.tsx
@@ -0,0 +1,19 @@
+import "./DebatePhoto.css";
+import defaultDebatePhoto from "../../assets/debate_stock.png";
+
+interface DebatePhotoProps {
+    id?: string;
+    alt: string;
+}
+
+const DebatePhoto = ({ id = defaultDebatePhoto, alt }: DebatePhotoProps) => {
+    return (
+        <>
+            <div className="image-width">
+                <img src={id} alt={alt} className="limit-img responsive-img" />
+            </div>
+        </>
+    );
+};
+
+export default DebatePhoto;
