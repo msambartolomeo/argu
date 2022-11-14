b50e167788eed3fea402430c2cd2f24f4f947078
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-11-14 16:38:17 -0300

    PATCH: Fix textarea being input
    State is now handled outside of component

--- frontend/src/components/TextArea/TextArea.tsx
+++ frontend/src/components/TextArea/TextArea.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { ChangeEventHandler } from "react";
+import cn from "classnames";
 
-type Props = {
-    text?: string;
-};
+interface Props {
+    text: string;
+    value?: string;
+    error?: string;
+    onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+}
 
-function TextArea({ text }: Props) {
-    const [value, setValue] = useState(text || "");
-
+function TextArea({ text, value, error, onChange }: Props) {
     return (
         <div className="input-field">
-            <input
-                type="text"
+            <textarea
                 value={value}
-                onChange={(e) => setValue(e.target.value)}
+                onChange={onChange}
+                className={cn({
+                    invalid: error !== null,
+                    "materialize-textarea": true,
+                })}
             />
-            <label className="active">Text</label>
+            <label className="active">{text}</label>
+            {error && <span className="helper-text error">error</span>}
         </div>
     );
 }
