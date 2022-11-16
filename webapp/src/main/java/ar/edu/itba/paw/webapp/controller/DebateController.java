0805ce8a69affb453d4fca7652c14c6116641d6c
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-11-15 23:51:02 -0300

    FEATURE: Add POST, PATCH and DELETE methods to debates

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/DebateController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/DebateController.java
@@ -1,14 +1,17 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.services.DebateService;
+import ar.edu.itba.paw.model.Debate;
 import ar.edu.itba.paw.webapp.dto.DebateDto;
+import ar.edu.itba.paw.webapp.form.CreateDebateForm;
+import ar.edu.itba.paw.webapp.patches.DebatePatch;
+import ar.edu.itba.paw.webapp.patches.PATCH;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.validation.Valid;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,6 +27,8 @@ public class DebateController {
 
     @Context
     private UriInfo uriInfo;
+    @Context
+    private Authentication auth;
 
     @GET
     @Produces(value = {MediaType.APPLICATION_JSON})
@@ -38,4 +43,37 @@ public class DebateController {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
+    @POST
+    @Consumes({MediaType.MULTIPART_FORM_DATA})
+    public Response createDebate(@Valid CreateDebateForm form) {
+
+        final Debate debate = debateService.create(form.getTitle(),
+                form.getDescription(),
+                auth.getName(),
+                form.getIsCreatorFor(),
+                form.getOpponentUsername(),
+                form.getImage(),
+                form.getCategory());
+
+        return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(debate.getDebateId())).build()).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteDebate(@PathParam("id") final long id) {
+        debateService.deleteDebate(id, auth.getName());
+
+        return Response.noContent().build();
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response patchDebate(@PathParam("id") final long id, final DebatePatch patch) {
+        if (patch.isConclusion()) {
+            debateService.startConclusion(id, auth.getName());
+        }
+
+        return Response.noContent().build();
+    }
 }
