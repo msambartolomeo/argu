2d18a68ef56ecc0c2014f23ad895a1bc3ef1d1fe
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-11-26 17:50:23 -0300

    CHORE: Add vote validation

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validators/ValidVoteValidator.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.paw.webapp.validators;
+
+import ar.edu.itba.paw.model.enums.DebateVote;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+
+public class ValidVoteValidator implements ConstraintValidator<ValidVote, String> {
+
+    @Override
+    public void initialize(ValidVote constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String vote, ConstraintValidatorContext context) {
+        return vote == null || Arrays.stream(DebateVote.values()).anyMatch((v) -> v.getName().equals(vote));
+    }
+}
