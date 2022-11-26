6a51677814c228f2fd8b17ba556f4ba4d223f513
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-11-26 17:51:02 -0300

    CHORE: fix vote service

--- model/src/main/java/ar/edu/itba/paw/model/exceptions/DebateAlreadyDeletedException.java
+++ model/src/main/java/ar/edu/itba/paw/model/exceptions/DebateAlreadyDeletedException.java
@@ -1,7 +1,8 @@
 package ar.edu.itba.paw.model.exceptions;
 
-public class VotingForbiddenException extends Exception403{
-    public VotingForbiddenException() {
-        super("Voting has been disabled due to closed or deleted debate", "error.forbidden.voting");
+// TODO: Check 400 vs 409
+public class DebateAlreadyDeletedException extends Exception400{
+    public DebateAlreadyDeletedException() {
+        super("Voting has been disabled due to closed or deleted debate", "error.already-deleted.debate");
     }
 }
