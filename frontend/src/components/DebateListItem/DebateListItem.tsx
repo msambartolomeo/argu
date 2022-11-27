6346a9a888ff426b2ec24e23a7d2ceea65798278
Azul <azkim@itba.edu.ar>
2022-11-27 19:04:26 -0300

    FEATURE: Add Debate List Item and User and Debate models

--- frontend/src/components/DebateListItem/DebateListItem.tsx
+++ frontend/src/components/DebateListItem/DebateListItem.tsx
@@ -1,5 +1,59 @@
-const DebateListItem = () => {
-    return <div>Item</div>;
+import Debate from "../../types/Debate";
+import Chip from "../Chip/Chip";
+import "./DebateListItem.css";
+import defaultDebatePhoto from "../../assets/debate_stock.png";
+
+interface DebateListItemProps {
+    debate: Debate;
+}
+
+// TODO: Check if creator is received like a string or an URI
+const DebateListItem = ({ debate }: DebateListItemProps) => {
+    return (
+        <div className="list-item">
+            <a
+                className="card black-text hoverable"
+                href={"/debates/" + debate.id}
+            >
+                <div className="card-content debate-info-holder">
+                    <div className="debate-holder-separator">
+                        <div className="debate-text-holder">
+                            <h5 className="debate-title word-wrap">
+                                {debate.name}
+                            </h5>
+                            <h6>
+                                Creator:{" "}
+                                {debate.creator.username !== null
+                                    ? debate.creator.username
+                                    : "[deleted user]"}
+                            </h6>
+                        </div>
+                        <div className="debate-footer">
+                            <Chip name={debate.category} />
+                            <Chip name={"Created: " + debate.createdDate} />
+                            <Chip name={"Status: " + debate.status} />
+                            <Chip
+                                name={
+                                    "Subscribed users: " + debate.subscriptions
+                                }
+                            />
+                        </div>
+                    </div>
+                    <div className="image-width">
+                        <img
+                            src={
+                                debate.image !== (null || undefined)
+                                    ? "/images/" + debate.image
+                                    : defaultDebatePhoto
+                            }
+                            alt="Debate photo"
+                            className="limit-img responsive-img"
+                        />
+                    </div>
+                </div>
+            </a>
+        </div>
+    );
 };
 
 export default DebateListItem;
---
+++ frontend/src/types/User.ts
@@ -0,0 +1,7 @@
+// TODO: Complete with missing fields
+
+export default interface User {
+    username: string;
+    email: string;
+    createdDate: string;
+}
