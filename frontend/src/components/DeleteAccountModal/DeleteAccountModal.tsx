32df72603d65622822024b6aba15a5c75a6e8e63
Azul <azkim@itba.edu.ar>
2022-11-30 15:48:13 -0300

    REFACTOR: Move forms to modals

--- frontend/src/components/DeleteAccountModal/DeleteAccountModal.tsx
+++ frontend/src/components/DeleteAccountModal/DeleteAccountModal.tsx
@@ -1,5 +1,47 @@
 import "./DeleteAccountModal.css";
-import DeleteConfirmationForm from "../DeleteConfirmationForm/DeleteConfirmationForm";
+
+const DeleteConfirmationForm = () => {
+    function handleSubmit(): void {
+        // console.log("Confirm submition of profile photo");
+    }
+
+    // TODO: Add error handling
+
+    return (
+        <form
+            method="delete"
+            acceptCharset="utf-8"
+            id="confirmationForm"
+            encType="multipart/form-data"
+            onSubmit={handleSubmit}
+        >
+            <div className="modal-content">
+                <h4>
+                    Are you sure you want to delete your account? This action
+                    cannot be undone.
+                </h4>
+                <div className="input-field">
+                    <label>Introduce password</label>
+                    <input type="password" className="validate" />
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a href="" className="modal-close waves-effect btn-flat">
+                    Close
+                </a>
+                <button
+                    className="modal-close waves-effect btn-flat"
+                    type="submit"
+                    form="confirmationForm"
+                    id="confirmationForm"
+                    name="deleteAccount"
+                >
+                    Confirm
+                </button>
+            </div>
+        </form>
+    );
+};
 
 const DeleteAccountModal = () => {
     return (
