ff79e22db597d8512f4157b562069550fe4a5c08
Azul <azkim@itba.edu.ar>
2023-01-29 18:03:35 -0300

    PATCH: Fix navigation to debate

--- frontend/src/components/DebateListItem/DebateListItem.tsx
+++ frontend/src/components/DebateListItem/DebateListItem.tsx
@@ -5,6 +5,7 @@ import defaultDebatePhoto from "../../assets/debate_stock.png";
 import { useTranslation } from "react-i18next";
 import "../../locales/index";
 import DebateDto from "../../types/dto/DebateDto";
+import { useNavigate } from "react-router-dom";
 
 interface DebateListItemProps {
     debate: DebateDto;
@@ -13,15 +14,20 @@ interface DebateListItemProps {
 // TODO: Check if creator is received like a string or an URI
 const DebateListItem = ({ debate }: DebateListItemProps) => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
 
     const debateCategory = t("discovery.categories." + debate.category);
     const debateStatus = t("debate.statuses." + debate.status);
+
+    const handleNavigateToDebate = () => {
+        navigate("/debate/" + debate.id);
+    };
 
     return (
         <div className="list-item">
             <a
                 className="card black-text hoverable"
-                href={"/debates/" + debate.id}
+                onClick={handleNavigateToDebate}
             >
                 <div className="card-content debate-info-holder">
                     <div className="debate-holder-separator">
