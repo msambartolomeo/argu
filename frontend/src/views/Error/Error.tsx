0fc3a3a83fd928e0e5d4c4db0153da1534141e7f
Azul <azkim@itba.edu.ar>
2023-02-02 18:17:53 -0300

    REFACTOR: Change code numbers for HttpStatusCode module codes

--- frontend/src/views/Error/Error.tsx
+++ frontend/src/views/Error/Error.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from "react-i18next";
 
 import { Link } from "react-router-dom";
 
+import { HttpStatusCode } from "axios";
+
 import error400 from "../../assets/error400.png";
 import error404 from "../../assets/error404.png";
 import error500 from "../../assets/error500.png";
@@ -9,18 +11,21 @@ import "./Error.css";
 
 interface ErrorProps {
     status: number;
-    message: string;
+    message?: string;
 }
 
-export const Error = ({ status, message }: ErrorProps) => {
+export const Error = ({
+    status,
+    message = "An error has ocurred",
+}: ErrorProps) => {
     const { t } = useTranslation();
 
     let image = "";
     switch (status) {
-        case 400:
+        case HttpStatusCode.BadRequest:
             image = error400;
             break;
-        case 404:
+        case HttpStatusCode.NotFound:
             image = error404;
             break;
         default:
