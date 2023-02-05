68b84b5a1427c1ce866a444009628c8fa992a1c3
Juan Negro <jnegro@itba.edu.ar>
2023-02-04 21:17:22 -0300

    FEATURE: Complete Login page fixing some merge conflicts

--- frontend/src/views/Login/Login.tsx
+++ frontend/src/views/Login/Login.tsx
@@ -1,11 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 
-import { FormEvent, useState } from "react";
-
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import InputField from "../../components/InputField/InputField";
 import SubmitButton from "../../components/SubmitButton/SubmitButton";
@@ -15,25 +13,66 @@ import "./Login.css";
 
 const Login = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const { t } = useTranslation();
 
     const { callLogin } = useLogin();
-    const [username, setUsername] = useState<string>();
-    const [password, setPassword] = useState<string>();
 
-    const handleRedirect = () => {
+    const schema = z.object({
+        username: z
+            .string()
+            .max(64, t("login.usernameTooLong") as string)
+            .min(1, t("login.usernameEmpty") as string),
+        password: z
+            .string()
+            .max(100, t("login.passwordTooLong") as string)
+            .min(1, t("login.passwordEmpty") as string),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(schema),
+    });
+
+    const handleRedirectRegister = () => {
         navigate("/register");
+    };
+
+    const handleLogin = (data: FieldValues) => {
+        callLogin(data.username, data.password);
+        // NOTE: Redirect to previous page if it exists, otherwise redirect to home
+        console.log(location);
+        location.state?.from
+            ? navigate(location.state.from, { replace: true })
+            : navigate("/", { replace: true });
     };
 
     return (
         <div className="card login-container">
-            <form method="post" acceptCharset="utf-8">
+            <form
+                acceptCharset="utf-8"
+                onSubmit={handleSubmit((data) => {
+                    handleLogin(data);
+                })}
+            >
                 <div className="card-content container-with-space">
                     <span className="card-title center">
                         {t("login.welcomeBack")}
                     </span>
-                    <InputField text={t("login.username")} />
-                    <InputField text={t("login.password")} type="password" />
+                    <InputField
+                        text={t("login.username")}
+                        register={register("username")}
+                        error={errors.username?.message?.toString()}
+                    />
+                    <InputField
+                        text={t("login.password")}
+                        type="password"
+                        register={register("password")}
+                        error={errors.password?.message?.toString()}
+                    />
                     <div className="left">
                         <label>
                             <input
@@ -50,7 +89,7 @@ const Login = () => {
             </form>
             <h6 className="center">
                 {t("login.noAccount")}&nbsp;
-                <a onClick={handleRedirect}>{t("login.register")}</a>
+                <a onClick={handleRedirectRegister}>{t("login.register")}</a>
             </h6>
         </div>
     );
