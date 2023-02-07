fe96c0b25baa8ea63016bf00540cbab96f6fab12
Juan Negro <jnegro@itba.edu.ar>
2023-02-07 19:32:43 -0300

    PATCH: Add body to patch requests

--- frontend/src/hooks/requests/usePatch.ts
+++ frontend/src/hooks/requests/usePatch.ts
@@ -6,12 +6,14 @@ export const usePatch = () => {
 
     async function callPatch(
         url: string,
+        inData: Record<string, unknown>,
         headers?: Record<string, string>,
         requiresAuth?: boolean
     ) {
         return await requestApi({
             url: url,
             method: RequestMethod.PATCH,
+            body: inData,
             headers: headers,
             requiresAuth: requiresAuth,
         });
