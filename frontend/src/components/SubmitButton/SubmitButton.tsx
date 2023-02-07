750246c591aa07980b1133d2641aa0b464a95d7c
Juan Negro <jnegro@itba.edu.ar>
2023-02-07 20:03:09 -0300

    FEATURE: Add disabled prop to SubmitButton

--- frontend/src/components/SubmitButton/SubmitButton.tsx
+++ frontend/src/components/SubmitButton/SubmitButton.tsx
@@ -1,11 +1,16 @@
 interface ButtonProps {
     text: string;
+    disabled?: boolean;
 }
 
-const SubmitButton = ({ text }: ButtonProps) => {
+const SubmitButton = ({ text, disabled }: ButtonProps) => {
     return (
         <>
-            <button type="submit" className="btn waves-effect center-block">
+            <button
+                type="submit"
+                className="btn waves-effect center-block"
+                disabled={disabled}
+            >
                 {text}
                 <i className="material-icons right">send</i>
             </button>
