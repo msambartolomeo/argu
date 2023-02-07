e8a7234b6db541e7ec9d44c67571292d12dc6f33
Juan Negro <jnegro@itba.edu.ar>
2023-02-07 20:03:32 -0300

    FEATURE: Add register prop to TextArea

--- frontend/src/components/TextArea/TextArea.tsx
+++ frontend/src/components/TextArea/TextArea.tsx
@@ -1,15 +1,17 @@
 import { ChangeEventHandler } from "react";
 
 import cn from "classnames";
+import { UseFormRegisterReturn } from "react-hook-form/dist/types/form";
 
 interface Props {
     text: string;
     value?: string;
     error?: string;
     onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+    register?: UseFormRegisterReturn;
 }
 
-function TextArea({ text, value, error, onChange }: Props) {
+function TextArea({ text, value, error, onChange, register }: Props) {
     return (
         <div className="input-field">
             <textarea
@@ -19,6 +21,7 @@ function TextArea({ text, value, error, onChange }: Props) {
                     invalid: error !== undefined,
                     "materialize-textarea": true,
                 })}
+                {...register}
             />
             <label className="input-label active">{text}</label>
             {error && <span className="helper-text error">{error}</span>}
