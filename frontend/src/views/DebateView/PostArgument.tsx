c2f40bf08b20e5cf7cb5bf3c901fac22b729b35e
Azul <azkim@itba.edu.ar>
2023-02-09 19:19:56 -0300

    CHORE: Remove unused imports

--- frontend/src/views/DebateView/PostArgument.tsx
+++ frontend/src/views/DebateView/PostArgument.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { HttpStatusCode } from "axios";
 import { useForm } from "react-hook-form";
