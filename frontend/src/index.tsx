583a078943504b90b524c5ae17a76e13e15b00cc
Juan Negro <jnegro@itba.edu.ar>
2023-02-11 19:18:17 -0300

    FEATURE: Add ConfiguredSnackbarProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import ConfiguredSnackbarProvider from "./ConfiguredSnackbarProvider";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <ConfiguredSnackbarProvider>
+            <App />
+        </ConfiguredSnackbarProvider>
     </React.StrictMode>
 );
 
