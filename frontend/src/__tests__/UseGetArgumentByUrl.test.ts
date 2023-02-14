fa36abe76aab9f0bafea29681f9a7d79bda4950c
Azul <azkim@itba.edu.ar>
2023-02-14 07:46:21 -0300

    CHORE: Add not found case for useGetArgumentByUrl

--- frontend/src/__tests__/UseGetArgumentByUrl.test.ts
+++ frontend/src/__tests__/UseGetArgumentByUrl.test.ts
@@ -1,3 +1,5 @@
+import { HttpStatusCode } from "axios";
+
 import { renderHook } from "@testing-library/react";
 
 import { mockedArgument1 } from "../__mocks__/mockedData";
@@ -18,7 +20,7 @@ describe("useGetArgumentByUrl", () => {
     it("should return the argument when the response status is HttpStatusCode.Ok", async () => {
         const mockArgumentDto = mockedArgument1;
         const mockResponse = {
-            status: 200,
+            status: HttpStatusCode.Ok,
             data: mockArgumentDto,
         };
         jest.spyOn(useGetModule, "useGet").mockReturnValueOnce({
@@ -34,6 +36,30 @@ describe("useGetArgumentByUrl", () => {
         expect(argumentByUrl).toEqual({
             status: mockResponse.status,
             data: mockResponse.data,
+        });
+        expect(result.current.loading).toBe(false);
+    });
+
+    it("should return status code 404 with message Argument not found when the response status is NotFound", async () => {
+        const mockResponse = {
+            status: HttpStatusCode.NotFound,
+            data: {
+                message: "Argument not found",
+            },
+        };
+        jest.spyOn(useGetModule, "useGet").mockReturnValueOnce({
+            loading: false,
+            callGet: jest.fn().mockResolvedValueOnce(mockResponse),
+        });
+
+        const { result } = renderHook(() => useGetArgumentByUrl());
+
+        const { getArgumentByUrl } = result.current;
+        const argumentByUrl = await getArgumentByUrl({ url: "/test" });
+
+        expect(argumentByUrl).toEqual({
+            status: mockResponse.status,
+            message: mockResponse.data.message,
         });
         expect(result.current.loading).toBe(false);
     });
